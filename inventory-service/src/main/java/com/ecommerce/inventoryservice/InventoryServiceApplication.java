63e198a8b7490fba08b7a57e0656f0352781da94
okmahipk-cmyk <okmahipk@gmail.com>
2026-02-24 09:23:32 +0000

    Add inventory-service and module structure

---
+++ inventory-service/src/main/java/com/ecommerce/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,18 @@
+package com.ecommerce.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(
+        exclude = {
+                org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+        }
+)
+@EnableDiscoveryClient
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+}
---
+++ order-service/src/main/java/com/ecommerce/orderservice/OrderServiceApplication.java
@@ -0,0 +1,17 @@
+package com.ecommerce.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(
+        exclude = {
+                org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+        }
+)
+public class OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+}
