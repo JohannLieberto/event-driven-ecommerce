643272e0d7c7e8bfe8a51cd48447c653a5415a59
JohannLieberto <67495488+JohannLieberto@users.noreply.github.com>
2026-04-05 15:56:56 +0100

    fix(karate): fix KarateTestRunner - use classpath root path and return Karate object for JUnit5 registration

--- karate-tests/src/test/java/com/ecommerce/karate/KarateTestRunner.java
+++ karate-tests/src/test/java/com/ecommerce/karate/KarateTestRunner.java
@@ -1,15 +1,13 @@
 package com.ecommerce.karate;
 
 import com.intuit.karate.junit5.Karate;
-import org.junit.jupiter.api.Test;
 
 public class KarateTestRunner {
 
-    @Test
-    void testAll() {
-        Karate.run("classpath:features")
-            .relativeTo(getClass())
-            .outputHtmlReport(true)
-            .outputJunitXml(true);
+    @Karate.Test
+    Karate testAll() {
+        return Karate.run("classpath:features")
+                     .outputHtmlReport(true)
+                     .outputJunitXml(true);
     }
 }
